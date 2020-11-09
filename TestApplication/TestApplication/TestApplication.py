9562fba134faafcc9d258af57af20a8e55bd3678
Devin Smith <devin.smith.biz@gmail.com>
2020-11-08 19:36:17 -0700

    updated test

--- TestApplication/TestApplication/TestApplication.py
+++ TestApplication/TestApplication/TestApplication.py
@@ -5,7 +5,7 @@ age = int(input('Please enter your age: '))
 factor = 2
 finalAge = age + factor
 multAge = age * factor
-divAge = age / factor
+divAge = float(age / factor)
 
 print('In', factor, 'years you will be', finalAge, 'years old', userName)
 print('Your age multiplied by', factor, 'is', multAge)
