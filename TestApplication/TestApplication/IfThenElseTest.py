8e40c52a89c8aa877a49a765d9edf6e5a0716185
Devin Smith <devin.smith.biz@gmail.com>
2020-11-08 20:45:57 -0700

    Updated test

--- TestApplication/TestApplication/IfThenElseTest.py
+++ TestApplication/TestApplication/IfThenElseTest.py
@@ -4,6 +4,7 @@ rawAge = int(input("Please tell me your age. "))
 if rawAge >= 21:
     print(name, "Your allowed to come in!")
     print("What would you like to drink?")
+    print("I made a change here")
 elif rawAge >=18:
     print(name, "Your are allowed to come in!")
     print("However, you are not allowed to drink! Please feel free to enjoy everything else")
