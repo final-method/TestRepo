912e07b867a7449f6a88b66a84a0a22e7dd3fd98
Devin Smith <devin.smith.biz@gmail.com>
2020-11-08 19:13:55 -0700

    Updated test

--- TestApplication/TestApplication/TestApplication.py
+++ TestApplication/TestApplication/TestApplication.py
@@ -1,1 +1,12 @@
+userName = input('Please enter your name:')
+print('Hello', userName + '!')
+age = int(input('Please enter your age: '))
 
+factor = 2
+finalAge = age + factor
+multAge = age * factor
+divAge = age / factor
+
+print('In', factor, 'years you will be', finalAge, 'years old', userName)
+print('Your age multiplied by', factor, 'is', multAge)
+print('Your age divided by', factor, 'is', divAge)
